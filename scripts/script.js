2b6dcb478409b02320906d21ca0ddaecc3769402
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2021-10-10 16:10:16 +0500

    added particle js on background

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  const STEP_COUNT = 15;
+  let currentStep = 1;
+
+  const updateBtnStates = () => {
+    if (currentStep == 1) {
+      $("#previousBtn").prop("disabled", true);
+    } else {
+      $("#previousBtn").attr("disabled", false);
+    }
+
+    if (currentStep == STEP_COUNT) {
+      $("#nextBtn").prop("disabled", true);
+    } else {
+      $("#nextBtn").attr("disabled", false);
+    }
+  };
+
+  // disable all steps except first on page load
+  $(".step").hide();
+  $("#step1").show();
+  updateBtnStates();
+
+  $("#nextBtn").on("click", () => {
+    if (currentStep < STEP_COUNT) {
+      $(`#step${currentStep}`).hide();
+      currentStep++;
+      $(`#step${currentStep}`).show();
+    }
+    updateBtnStates();
+  });
+
+  $("#previousBtn").on("click", () => {
+    if (currentStep > 1) {
+      $(`#step${currentStep}`).hide();
+      currentStep--;
+      $(`#step${currentStep}`).show();
+    }
+    updateBtnStates();
+  });
+});
